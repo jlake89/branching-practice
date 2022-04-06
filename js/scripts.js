9eb59ac6b3c954242f4643fdfad6f34fe59d4388
Joel Lake <joelcooperlake@gmail.com>
2022-04-05 20:45:28 -0700

    WIP

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function())
+  $("celeb-dating").submit(function(event) {
+    const party = $("#party").val();
+    const age = parseInt($("#age").val());
+    const dates = $("dates").val();
+  })
+if (party === "club") {
+$('showThisThing').show();
+}
